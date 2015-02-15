b1185c386b606c0e9d09909f38d216b62b839be1
kwikiel <random8902@gmail.com>
2015-02-15 11:31:46 -0500

    Timestamp

--- diary.py
+++ diary.py
@@ -1,3 +1,6 @@
+import time
+
+current_time = time.strftime("%d.%m.%y %H:%M ", time.localtime())
 with open("hello.txt", "a") as myInput:
     userInput = raw_input("What you did today? "+"\n")
-    myInput.writelines(userInput+'\n')
+    myInput.writelines(current_time + userInput+'\n')
